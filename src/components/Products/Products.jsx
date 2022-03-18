c7ae2bcc0d8ab954cbde893601f06678cb2eea7d
D-Gar27 <htetwinkhant999@gmail.com>
2022-03-18 11:50:35 +0630

    add json-sever

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,13 +1,47 @@
+import { useEffect, useState } from 'react';
 import Sidebar from '../Sidebar/Sidebar';
 import './Products.scss';
+import { BsHeart } from 'react-icons/bs';
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await fetch(
+          'http://localhost:3001/products?_page=2&_limit=15'
+        );
+        const data = await res.json();
+        console.log(data);
+        setProducts(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchProducts();
+  }, []);
   return (
     <main className="products_page">
       <h2 className="products_title">PRODUCTS</h2>
       <div className="container products_page_container">
         <Sidebar />
-        <div></div>
+        <section className="products_container">
+          {products &&
+            products.map((product) => {
+              return (
+                <article className="product_article" key={product?.id}>
+                  <img src={product?.image} alt="" className="product_img" />
+                  <div className="info">
+                    <p className="product_name">{product?.name}</p>
+                    <p className="product_price">{product?.price?.max} MMK</p>
+                  </div>
+                  <button className="heart_icon">
+                    <BsHeart />
+                  </button>
+                </article>
+              );
+            })}
+        </section>
       </div>
     </main>
   );
